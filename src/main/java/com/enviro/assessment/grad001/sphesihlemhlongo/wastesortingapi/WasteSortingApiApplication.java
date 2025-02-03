1cdf05885ff10ec4e09f412c0a3a1f1d8f373292
Sphesihle Mhlongo <mhlongosihle49@gmail.com>
2025-02-03 06:49:54 +0200

    first commit

---
+++ src/main/java/com/enviro/assessment/grad001/sphesihlemhlongo/wastesortingapi/WasteSortingApiApplication.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.sphesihlemhlongo.wastesortingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WasteSortingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WasteSortingApiApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/enviro/assessment/grad001/sphesihlemhlongo/wastesortingapi/model/WasteCategory.java
@@ -0,0 +1,4 @@
+package com.enviro.assessment.grad001.sphesihlemhlongo.wastesortingapi.model;
+
+public class WasteCategory {
+}
